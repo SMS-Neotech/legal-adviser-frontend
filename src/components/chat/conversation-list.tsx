6e791bdcc6740d587da699b438cbeb617760ff2d
Madan Mohatra <mohatra.mk8@gmail.com>
2025-11-09 04:14:43 +0000

    Try fixing this error: `Runtime Error: Error: AlertDialogTrigger is not

--- src/components/chat/conversation-list.tsx
+++ src/components/chat/conversation-list.tsx
@@ -30,6 +30,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { cn } from "@/lib/utils";
 import { useTranslation } from "../language-provider";
